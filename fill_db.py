8c3114685fdd699c49df0729044ab30f6bb678b0
Tyler Watkins <tylerwatkins101@gmail.com>
2020-02-04 15:19:34 -0300

    Add files via upload

--- fill_db.py
+++ fill_db.py
@@ -1,17 +1,17 @@
 from models import engine, db_session, Base, Basic, Education, Occupation, Household, Foodandbeverage, Hobbiesandinterests
 Base.metadata.create_all(bind=engine)
 
-tyler = Basic(name='Tyler', age=34, gender='male')
-db_session.add(tyler)
-degree = Education(highestdegree='Masters', areaofstudy='Computer Science', basic=tyler)
+melanie = Basic(name='Melanie', age=34, gender='female')
+db_session.add(melanie)
+degree = Education(highestdegree='Masters', areaofstudy='Petroleum Engineering', basic=melanie)
 db_session.add(degree)
-job = Occupation(organization='Prifina', employeedepartment='Information Technology', employeeprimaryrole='Data Analyst', basic=tyler)
+job = Occupation(organization='ExxonMobil', employeedepartment='Petrophysics', employeeprimaryrole='Data Analyst', basic=melanie)
 db_session.add(job)
-house = Household(numberofpersonsinhome=3, maritalstatus='Married', numberofchildren=1, accomodationtype='Apartment', income='over 120,000', basic=tyler)
+house = Household(numberofpersonsinhome=3, maritalstatus='Married', numberofchildren=1, accomodationtype='Apartment', income='over 120,000', basic=melanie)
 db_session.add(house)
-foods = Foodandbeverage(fastfoodweeklyfrequency=0, alcoholweeklyfrequency=3, restaurantweeklyfrequency=3, basic=tyler)
+foods = Foodandbeverage(fastfoodweeklyfrequency=0, alcoholweeklyfrequency=3, restaurantweeklyfrequency=3, basic=melanie)
 db_session.add(foods)
-hobbies = Hobbiesandinterests(cinemaweeklyfrequency = 1, exerciseweeklyfrequency = 5, favoritemusicgenre = 'rock', basic=tyler)
+hobbies = Hobbiesandinterests(cinemaweeklyfrequency = 1, exerciseweeklyfrequency = 5, favoritemusicgenre = 'rock', basic=melanie)
 db_session.add(hobbies)
 
 
